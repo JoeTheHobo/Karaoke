a53d8bdece88e20dddc072281807bcf264140839
JoeTheHobo <johnjonesma@gmail.com>
2026-01-14 15:40:23 -0700

    asdf

--- public/songStats.js
+++ public/songStats.js
@@ -775,7 +775,7 @@ let songStats = {
     "extension": "Lyrics"
   },
   "-gs_SZM2lRk": {
-    "plays": 17,
+    "plays": 18,
     "song": "do re mi ",
     "artist": "blackbear",
     "url": "https://www.youtube.com/watch?v=-gs_SZM2lRk",
